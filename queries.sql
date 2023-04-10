5c5eb1fa9dba83c93420dcfafa12cb0b14f2e48b
elijah-g-14 <69686507+elijah-g-14@users.noreply.github.com>
2023-04-10 14:07:58 -0400

    Added other procedures

--- queries.sql
+++ queries.sql
@@ -1,15 +1,9 @@
 use library;
 
-DROP PROCEDURE IF EXISTS GetBooks; 
 DROP PROCEDURE IF EXISTS GetByCategory;
 DROP PROCEDURE IF EXISTS GetByYear;
-
-DELIMITER //
-CREATE PROCEDURE GetBooks()
-BEGIN
-    SELECT id, title FROM books_Book LIMIT 10;
-END//
-DELIMITER ;
+DROP PROCEDURE IF EXISTS GetByAuthor;
+DROP PROCEDURE IF EXISTS GetByLangYear;
 
 DELIMITER //
 
@@ -40,4 +34,43 @@ BEGIN
 END//
 
 DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE GetByAuthor(IN author_name VARCHAR(255))
+BEGIN
+    SELECT B.id, B.title, A.name AS author, F.name AS format, GROUP_CONCAT(C.name SEPARATOR ', ') AS categories
+    FROM books_Book B
+    INNER JOIN books_BookAuthors BA ON B.id = BA.book_id
+    INNER JOIN books_Author A ON BA.author_id = A.id
+    INNER JOIN books_Format F ON B.format_id = F.id
+    INNER JOIN books_BookCategories BC ON B.id = BC.book_id
+    INNER JOIN books_Category C ON BC.category_id = C.id
+    WHERE A.name LIKE CONCAT('%', author_name, '%')
+    GROUP BY B.id
+    LIMIT 10;
+END//
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE GetByLangYear(IN lang VARCHAR(255), IN year INT)
+BEGIN
+    SELECT B.id, B.title, A.name AS author, F.name AS format, C.name AS category
+    FROM books_Book B
+    INNER JOIN books_BookAuthors BA ON B.id = BA.book_id
+    INNER JOIN books_Author A ON BA.author_id = A.id
+    INNER JOIN books_Format F ON B.format_id = F.id
+    INNER JOIN books_BookCategories BC ON B.id = BC.book_id
+    INNER JOIN books_Category C ON BC.category_id = C.id
+    WHERE YEAR(B.publication_date) = year AND B.lang = lang
+    LIMIT 10;
+END//
+
+DELIMITER ;
+
+
+
+
 
